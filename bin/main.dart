21600e4da72dfa7fccdafca18a1ccac6df893920
bmsrangel <bmsrangel@hotmail.com>
2020-09-22 23:03:23 -0300

    Removed comments from main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -3,5 +3,4 @@ import 'package:criptografia_teste/criptografia.dart';
 void main(List<String> arguments) {
   print(
       'Hello world: ${Criptografia.criptografar("jefferson.santos@expertsystem.com.br".trim())}!');
-  // print('Hello world: ${Criptografia.decriptografar("Js3423@!")}!');
 }
